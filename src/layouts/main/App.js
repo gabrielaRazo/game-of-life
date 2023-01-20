dec4cbd661508f6d2b4e05c52efae0436ae8f22e
Gabriela <gabriela.razo@painani.mx>
2023-01-20 13:56:43 -0600

    Add: React Router DOM library

--- src/layouts/main/App.js
+++ src/layouts/main/App.js
@@ -1,19 +1,15 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Container from '../../pages/container/container';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Container />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/container/container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Grid from '../../components/grid/grid';
+
+const Container = () => {
+  return (
+    <div>
+      <Grid />
+    </div>
+  );
+};
+
+export default Container;
