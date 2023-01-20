b9a169a7298b0939fba162dd59f0852c52ca1a44
Gabriela <gabriela.razo@painani.mx>
2023-01-20 16:45:50 -0600

    style: Responsiveness using Bootstrap
    Col with span and offset help to position the component

--- src/components/grid/grid.js
+++ src/components/grid/grid.js
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import './grid.css';
 
 const Grid = () => {
@@ -19,19 +23,34 @@ const Grid = () => {
   }, []);
 
   return (
-    <div>
-      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        {grid &&
-          grid.map((rows, i) =>
-            rows.map((cols, j) => (
-              <div
-                className="tableStyle"
-                style={{ backgroundColor: grid[i][j] ? 'pink' : 'white' }}
-              />
-            ))
-          )}
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <Col
+          lg={{ span: 6, offset: 3 }}
+          md={{ span: 6, offset: 1 }}
+          xs={{ span: 12, offset: 0 }}
+        >
+          <div
+            style={{
+              display: 'grid',
+              gridTemplateColumns: `repeat(${numCols}, 20px)`,
+            }}
+          >
+            {grid &&
+              grid.map((rows, i) =>
+                rows.map((cols, j) => (
+                  <div
+                    className="tableStyle"
+                    style={{
+                      backgroundColor: grid[i][j] ? 'pink' : 'white',
+                    }}
+                  />
+                ))
+              )}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
