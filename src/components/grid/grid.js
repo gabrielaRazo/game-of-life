d5d0f78e7a4d9f61dc17c1f26c40fe17e57eada8
Gabriela Razo Roldán <53321809+gabrielaRazo@users.noreply.github.com>
2023-01-23 01:55:11 +0000

    add: Funtion to start the game
    The updateGrid will update the state to start playing the game

--- src/components/grid/grid.js
+++ src/components/grid/grid.js
@@ -1,14 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
 
 import './grid.css';
 
 const Grid = () => {
+  const numRows = 30;
+  const numCols = 30;
   const [grid, setGrid] = useState([]);
-  const [numCols, setNumCols] = useState(30);
-  const [numRows, setNumRows] = useState(30);
+  const [running, setRunning] = useState(false);
+  const runningRef = useRef(running);
+
+  useEffect(() => {
+    runningRef.current = running;
+  }, [running]);
 
   const onEmptyGrid = () => {
     var grid = [];
@@ -34,7 +42,6 @@ const Grid = () => {
 
   const onColorCell = (i, j) => {
     const newGrid = JSON.parse(JSON.stringify(grid));
-    console.log('newGrid', newGrid);
     // Toggle the cell between "alive" and "dead"
     newGrid[i][j] = grid[i][j] === 1 ? 0 : 1;
     setGrid(newGrid);
@@ -42,12 +49,61 @@ const Grid = () => {
 
   useEffect(() => {
     onEmptyGrid();
-    //setGrid(randomGrid());
   }, []);
+
+  const updateGrid = (grid) => {
+    if (!runningRef.current) {
+      return;
+    }
+    let newGrid = JSON.parse(JSON.stringify(grid));
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        let aliveNeighbors = 0;
+        for (let x = -1; x <= 1; x++) {
+          for (let y = -1; y <= 1; y++) {
+            if (x === 0 && y === 0) continue; //current cell does not change its state
+            if (i + x < 0 || j + y < 0) continue; //check if the indexes are not negative
+            if (i + x >= grid.length || j + y >= grid[0].length) continue; //check if the indexes are not greater than the grid itself
+
+            if (grid[i + x][j + y] === 1) {
+              //check if the cell is alive
+              aliveNeighbors++;
+            }
+          }
+        }
+        //rules for the game of life
+        if (grid[i][j] === 1) {
+          if (aliveNeighbors < 2 || aliveNeighbors > 3) {
+            //cell dies
+            newGrid[i][j] = 0;
+          }
+        } else {
+          if (aliveNeighbors === 3) {
+            //cell comes to life
+            newGrid[i][j] = 1;
+          }
+        }
+      }
+    }
+    return newGrid;
+  };
+
+  useEffect(() => {
+    let intervalId;
+    if (running) {
+      intervalId = setInterval(() => {
+        setGrid(updateGrid);
+      }, 500);
+    } else {
+      clearInterval(intervalId);
+    }
+    return () => clearInterval(intervalId);
+  }, [running, setGrid, updateGrid]);
 
   return (
     <Container>
       <Row>
+        <div className="space" />
         <Col
           lg={{ span: 6, offset: 3 }}
           md={{ span: 6, offset: 1 }}
@@ -77,6 +133,24 @@ const Grid = () => {
                 ))
               )}
           </div>
+          <div className="space" />
+          <Stack direction="horizontal" gap={3}>
+            <Button
+              variant="outline-secondary"
+              onClick={() => setRunning(!running)}
+            >
+              {running ? 'Stop' : 'Start'}
+            </Button>
+            <Button variant="outline-secondary" onClick={() => onEmptyGrid()}>
+              Restart
+            </Button>
+            <Button
+              variant="outline-secondary"
+              onClick={() => setGrid(randomGrid())}
+            >
+              Random
+            </Button>
+          </Stack>
         </Col>
       </Row>
     </Container>
